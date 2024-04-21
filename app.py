14d90671d8a0ca2a954dd5ffa62e97c237943b0e
unknown <ajiqdin646@gmail.com>
2024-04-21 16:03:13 +0800

    add backend tech

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask , render_template , flash , redirect , url_for
 from form import RegistrationForm , Loginform
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+        id = db.Column(db.Integer , primary_key = True)
+        username = db.Column(db.String(20) , unique = True , nullable = False)
+        email = db.Column(db.String(120) , unique = True , nullable = False)
+        password = db.Column(db.String(120) , nullable = False)
+   
+        def __repr__(self):
+           return f'User("{self.username}" , {self.email})'
+        
+        with app.app_context(): db.create_all()
+
+
+  
+
 
 
 
@@ -37,6 +56,7 @@ def register():
         flash(f'Account created for {form.username.data} !')
         return redirect(url_for('home'))
     return render_template('register.html' , form = form)
+
 
 
 if __name__ == '__main__':

b83fd033d9ee10557b3da3ff3247a3ab99b495da
unknown <ajiqdin646@gmail.com>
2024-04-23 17:59:35 +0800

    add current_user authentication

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,7 @@ from flask import render_template , flash , redirect , url_for
 from application.form import RegistrationForm , Loginform
 from application.models import User
 from application import app , db , bcrypt
-from flask_login import login_user
+from flask_login import login_user , current_user , logout_user
 
 
 @app.route('/')
@@ -15,6 +15,8 @@ def home():
 
 @app.route('/login' , methods = ['POST' , 'GET'])
 def login():
+    if current_user.is_authenticated:
+         return redirect(url_for('home'))
     form = Loginform()
     if form.validate_on_submit():
             flash(f'Login Successful !')
@@ -30,6 +32,8 @@ def login():
 @app.route('/register' , methods = ['POST' , 'GET'])
 
 def register():
+    
+         
     form = RegistrationForm()
 
     if form.validate_on_submit():
@@ -40,3 +44,11 @@ def register():
         flash(f'Account created for {form.username.data} !' , 'success!')
         return redirect(url_for('login'))
     return render_template('register.html' , form = form)
+
+
+@app.route('/logout')
+def logout():
+     logout_user()
+     return redirect(url_for('home'))
+
+     
